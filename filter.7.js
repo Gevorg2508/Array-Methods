f9854e75b0afd34d249d4b089962ea74bf2a509e
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-09 16:22:31 +0400

    feat: how arr.filter method works

---
+++ filter.7.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5,5,6,4,4,3,3,3443];
+arr.filter(elem => elem <200);
+console.log(arr);
+let newArr = arr.filter(elem => elem <200);
+console.log(newArr);
+console.log(arr.filter(elem => elem <200));
