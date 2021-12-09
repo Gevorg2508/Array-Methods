19f6dfa46332318051592d2f29923ea78face00c
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-09 16:56:43 +0400

    feat: how works arr.reduce() method

---
+++ reduce.10.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5,6,7];
+arr.reduce((akk, curr) => akk +curr,0);
+console.log(arr);
+let newArr = arr.reduce((akk, curr) => akk +curr,0);
+console.log(newArr);
+console.log(arr.reduce((akk, curr) => akk +curr,0));
